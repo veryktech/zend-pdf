770f821892286f69a8a954c75ff65e8cd115ceb1
avzer <ghostwei@gmail.com>
2024-11-04 15:15:16 +0800

    默认的

--- library/Zend/Pdf/StringParser.php
+++ library/Zend/Pdf/StringParser.php
@@ -514,7 +514,7 @@ class Zend_Pdf_StringParser
             return null;
         }
 
-        $ref = new Zend_Pdf_Element_Reference((int)$objNum, (int)$genNum, $this->_context, $this->_objFactory->resolve());
+        $ref = new Zend_Pdf_Element_Reference((int)$objNum, $this->_context, $this->_objFactory->resolve(), (int)$genNum);
 
         return $ref;
     }
